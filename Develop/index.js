dce4ad2ed8f4cc2e28d67a450805076fcc37e935
Tanya Y <brulek7@gmail.com>
2021-04-20 21:53:38 -0400

    create code in index and generateMarkdown, deleted Develop file and added output file

--- Develop/index.js
+++ Develop/index.js
@@ -1,81 +1,158 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
-const fs = require('fs');
-const axios = require('axios');
-const generate = require("./utils/generateMarkdown");
+const fs = require("fs"); //Node standard library package for reading and writing files
+// const axios = require('axios');
+
+const util = require("util");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [{
+const questions = [
+  {
     type: "input",
     name: "title",
-    message: "What is your project title?"
-},
-{
+    message: "What is your project title?",
+  },
+  {
     type: "input",
     name: "description",
-    message: "Please provide your project's description"
-},
-{
+    message: "Please provide your project's description",
+  },
+
+  {
+    type: "input",
+    name: "Table of Contents",
+    message: "Table of Contents",
+  },
+  {
     type: "input",
     name: "installation",
-    message: "Please provide the installation instructions"
-},
-{
+    message: "Please provide the installation instructions to run this app",
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "Which open source license would you like to use?",
+    choices: [
+      "MIT",
+      "Mozilla Public 2.0",
+      "GVL-GPL 3.0",
+      "APACHE 2.0",
+      "BSD 3",
+      "GNU AGPLv3.0",
+      "GNU GPLv2.0",
+      "GNU GPLv3.0",
+      "None",
+    ],
+  },
+  {
     type: "input",
-    name: "licence",
-    message: "Please provide the project licence or your badge link"
-},
-{
+    name: "usage",
+    message: "Please provide how to use this app",
+  },
+  {
     type: "input",
-    name: "usage information",
-    message: "Please provide how to use this app"
-},
-{
+    name: "contribution",
+    message: "Who contributed to this project?",
+  },
+  {
     type: "input",
-    name: "contribution guidelines",
-    message: "How the other people can contribute?"
-},
-{
+    name: "test",
+    message:
+      "Please provede test instructions, what command are need ti test this app?",
+  },
+  {
     type: "input",
-    name: "test instructions",
-    message: "Please provede test instructions?"
-},
-{
+    message: "Contact info for further questions",
+    name: "Questions",
+  },
+  {
     type: "input",
     name: "email",
-    message: "What's your email address??"
-},
-{
+    message: "What's your email address?",
+  },
+  {
     type: "input",
     name: "username",
-    message: "What is your GitHub username?"
-},
-{
-    type: "input",
-    name: "repo",
-    message: "What is your repo link?"
-}];
+    message: "What is your GitHub username?",
+
+    // {
+    //     type: "input",
+    //     name: "repo",
+    //     message: "What is your repo link?"
+    //
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    const file = generate(fileName, data)
- }
-
-// TODO: Create a function to initialize app
-function init() {
-    inquirer
-  .prompt(questions)
-  .then(answers => {
-      console.log(answers);
-  })
-  .catch(error => {
-    if(error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
+  fs.writeFile(fileName, data, function (err) {
+    console.log(fileName);
+    console.log(data);
+    if (err) {
+      return console.log(err);
     } else {
-      // Something else went wrong
+      console.log("New README file created with success!");
     }
   });
 }
 
-// Function call to initialize app
+function init() {
+  inquirer.prompt(questions).then(function (data) {
+    writeToFile("./output/README.md", generateMarkdown(data));
+    console.log(data);
+  });
+}
+
 init();
+
+// function init() {
+//   inquirer.prompt(questions).then((responses) => {
+//     // console.log(responses);
+//     writeToFile("./output/README.md", generateMarkdown({ ...responses }));
+//   });
+// }
+
+// init();
+
+// const file = generate(fileName, data)
+// writeFile(fileName, data, err => {
+//     if (err) {
+//       throw err;
+//     }
+//   });
+// }
+
+// TODO: Create a function to initialize app
+// function init() {
+//     inquirer
+//   .prompt(questions)
+//   .then(answers => {
+//       console.log(answers);
+
+//   })
+//   .catch(error =>{
+//     if(error.isTtyError) {
+//       // Prompt couldn't be rendered in the current environment
+//     } else {
+//       // Something else went wrong
+//     }
+//   });
+// }
+
+// function init() {
+//     prompt(questions).then(answers => {
+
+//         const response = generateMarkdown(answers);
+//         console.log(answers);
+
+// writeToFile("README.md", response);
+// console.log("New README file created with success!");
+
+//     })
+
+// }
+
+// Function call to initialize app
+
+// console.log("New README file created with success!");
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,49 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  
+  # ${data.title}
+  https://github.com/${data.username}/${data.title}
 
-`;
+
+  ## Table of Contents
+  -[Description](#description)
+  -[Installation](#installation)
+  -[License](#license)
+  -[Usage](#usage)
+  -[Contribution](#contribution)
+  -[Test](#test)
+
+
+  ## Description
+  ${data.description}
+
+  ## Installation
+  ${data.installation}
+
+  ## License
+  ${data.license}
+
+  ## Usage
+  ${data.usage}
+
+  ## Contribution
+  ${data.contribution}
+
+  ## Test
+  ${data.test}
+
+
+ ## Questions
+ For any additional information or questions find me at:
+
+  Email:[${data.email}](mailto:tatyana.yarushin@gmail.com)
+  Github:[${data.username}](https://github.com/TatyanaYarush)
+
+`
 }
 
 module.exports = generateMarkdown;
+
+
+
